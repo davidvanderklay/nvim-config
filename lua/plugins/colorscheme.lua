26f50c5c41cc7734b78d99c46dccd059c3dceef0
David Vanderklay <davidvanderklay@gmail.com>
2024-10-16 15:43:49 -0500

    kanagawa theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,14 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
+  { "rebelot/kanagawa.nvim" },
   -- { "sainnhe/gruvbox-material" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "kanagawa",
     },
   },
 }
